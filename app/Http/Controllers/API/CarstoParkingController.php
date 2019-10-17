c3eca653211e0287d84a5c700e4e1cfbdfced49d
Valeri Mladenov <valeri.mladenov10@gmail.com>
2019-10-17 23:01:29 +0300

    Carbon Issue

--- app/Http/Controllers/API/CarstoParkingController.php
+++ app/Http/Controllers/API/CarstoParkingController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 
 use App\CarsToParkings;
 use App\Http\Controllers\Controller;
+use App\Payments;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -13,34 +15,46 @@ class CarstoParkingController extends Controller
 
     public function index()
     {
-        $cars  = CarsToParkings::all();
-        return response()->json($cars);
+        $cars = CarsToParkings::join('parkings', "parking_id", "=", "parkings.id")->get();
+        $payment = Payments::where('code', $cars[0]->payment_code)->first();
+        $now = Carbon::now()->toTimeString();
+        $payment_time = $payment->created_at->format('H:i:s');
+        $paid_time = Carbon::make($payment->time)->toTimeString();
+        $time_left = ($now - $payment_time) - $paid_time;
+        return $time_left;
+
+        $data = [
+            'car_number' => $cars[0]->car_number,
+            'street' => $cars[0]->parking_street,
+            'payment_code' => $cars[0]->payment_code,
+            'payment_time' => $time_left,
+        ];
+        return response()->json($data);
     }
 
     public function post(Request $request)
     {
 
-        $validate =  Validator::make($request->all(),[
-           'car_number' => 'required|max:8',
-           'parking_id' => 'required',
+        $validate = Validator::make($request->all(), [
+            'car_number' => 'required|max:8',
+            'parking_id' => 'required',
             'parking_time' => 'request|max:120',
             'payment_code' => 'request',
         ]);
 
-        if($validate->fails()){
-            return response()->json(['error'=>"504"]);
-        }else{
-           try{
-               $new_car_to_parking = CarsToParkings();
-               $new_car_to_parking->car_number = $request->get('car_number');
-               $new_car_to_parking->parking_id = $request->get('parking_id');
-               $new_car_to_parking->parking_time = $request->get('parking_time');
-               $new_car_to_parking->payment_code = $request->get('payment_code');
-               $new_car_to_parking->save();
-               return response()->json(['success'=>true,'code'=>"200"]);
-           }
-            catch(Exception $e){
-                return response()->json(['error'=>"504"]);
+        if($validate->fails()) {
+            return response()->json(['error' => "504"]);
+        } else {
+            try {
+                $new_car_to_parking = CarsToParkings();
+                $new_car_to_parking->car_number = $request->get('car_number');
+                $new_car_to_parking->parking_id = $request->get('parking_id');
+                $new_car_to_parking->parking_time = $request->get('parking_time');
+                $new_car_to_parking->payment_code = $request->get('payment_code');
+                $new_car_to_parking->save();
+                return response()->json(['success' => true, 'code' => "200"]);
+            } catch(Exception $e) {
+                return response()->json(['error' => "504"]);
             }
         }
     }
---
+++ app/Payments.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payments extends Model
+{
+    protected $primaryKey = "id";
+    protected $table = "payments";
+    public $incrementing = true;
+    public $timestamps = true;
+    protected $hidden =['ids'];
+}
